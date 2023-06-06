367df4ccccbeae9361d5a78eed07f03535382b39
L1F1N <118181137+L1F1N@users.noreply.github.com>
2023-06-06 13:22:48 +0300

    Add files via upload

---
+++ pract5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите слово: ");
+            string word = Console.ReadLine();
+            bool proverka = true;
+            for(int i = 0; i < word.Length/2; i++)
+            {
+                if (word[i] != word[word.Length-i-1])
+                {
+                    proverka = false;
+                    break;
+                }
+            }
+            if (proverka)
+            {
+                Console.WriteLine("Введеное слово является перевертышем");
+            }
+            else
+            {
+                Console.WriteLine("Введеное слово не является перевертышем");
+            }
+        }
+    }
+}
---
+++ pract5_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != text[text.Length - i - 1])
+                {
+                    proverka = false;
+                    break;
+                } 
+            }
+            if (proverka)
+            {
+                Console.WriteLine("Введеное слово является перевертышем");
+            }
+            else
+            {
+                Console.WriteLine("Введеное слово не является перевертышем");
+            }
+        }
+    }
+}
